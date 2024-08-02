d6db9239c12c8a68629f61928c0bc3a2c02e4fb0
VopDev <142059852+VopDev@users.noreply.github.com>
2024-08-01 21:53:14 -0600

    Update vSerenityPosts.java

--- src/main/java/net/botwithus/vSerenityPosts.java
+++ src/main/java/net/botwithus/vSerenityPosts.java
@@ -1,40 +1,19 @@
 package net.botwithus;
 
-import net.botwithus.api.game.hud.inventories.Backpack;
 import net.botwithus.internal.scripts.ScriptDefinition;
 import net.botwithus.rs3.game.Client;
-import net.botwithus.rs3.game.actionbar.ActionBar;
-import net.botwithus.rs3.game.hud.interfaces.Component;
-import net.botwithus.rs3.game.hud.interfaces.Interfaces;
 import net.botwithus.rs3.game.minimenu.MiniMenu;
 import net.botwithus.rs3.game.minimenu.actions.ComponentAction;
 import net.botwithus.rs3.game.queries.builders.characters.NpcQuery;
-import net.botwithus.rs3.game.queries.builders.components.ComponentQuery;
-import net.botwithus.rs3.game.queries.builders.objects.SceneObjectQuery;
 import net.botwithus.rs3.game.scene.entities.characters.npc.Npc;
 import net.botwithus.rs3.game.scene.entities.characters.player.LocalPlayer;
-import net.botwithus.rs3.game.scene.entities.object.SceneObject;
 import net.botwithus.rs3.script.Execution;
 import net.botwithus.rs3.script.LoopingScript;
 import net.botwithus.rs3.script.config.ScriptConfig;
-import net.botwithus.rs3.game.Item;
-import net.botwithus.rs3.game.queries.builders.items.InventoryItemQuery;
-import net.botwithus.api.game.hud.inventories.Backpack;
-import net.botwithus.api.game.hud.inventories.Bank;
 import net.botwithus.rs3.events.impl.ChatMessageEvent;
-import net.botwithus.rs3.events.impl.ServerTickedEvent;
-import net.botwithus.rs3.game.Area;
-import net.botwithus.rs3.game.Coordinate;
-import net.botwithus.rs3.game.queries.results.EntityResultSet;
-import net.botwithus.rs3.game.queries.results.ResultSet;
-import net.botwithus.rs3.game.skills.Skills;
-import net.botwithus.rs3.util.Regex;
 
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-import java.util.List;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
+
 
 public class vSerenityPosts extends LoopingScript {
 
